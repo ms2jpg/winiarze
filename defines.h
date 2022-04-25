f932b7d6feb70fcd900b0365b70b03e420c8bd0a
MichalKwarta <Michu1164@gmail.com>
2022-04-25 21:47:58 +0200

    add inline printTime func to defines.h

--- defines.h
+++ defines.h
@@ -1,6 +1,6 @@
 #ifndef DEFINES_H
 #define DEFINES_H
-#include <time.h>
+
 
 // other defines go here
 #define WINEMAKERS_NUMBER 3
@@ -21,7 +21,18 @@ WINE_REQUEST_ACK,
 GRAB_WINE,
 };
 
-#define debug(FORMAT, ...) printf("\033[%d;%dm[c:%d][r:%d][%c]: " FORMAT "\033[0m\n", (*this->rank) % 2, 31 + ((*this->rank) / 2) % 7, *this->clock, *this->rank, ((*this->rank) < WINEMAKERS_NUMBER ? 'W' : 'S'), ##__VA_ARGS__);
+inline char* printTime()
+{
+    time_t rawtime;
+  struct tm * timeinfo;
+  char *buffer = new char[10];
+
+  time (&rawtime);
+  timeinfo = localtime (&rawtime);
+  strftime (buffer,10,"%H:%M:%S",timeinfo);
+     return buffer;
+};
+#define debug(FORMAT, ...) printf("\033[%d;%dm[c:%d][r:%d][%c][%s]: " FORMAT "\033[0m\n", (*this->rank) % 2, 31 + ((*this->rank) / 2) % 7, *this->clock, *this->rank, ((*this->rank) < WINEMAKERS_NUMBER ? 'W' : 'S'),printTime(), ##__VA_ARGS__);
 
 
-#endif // MY_DEFINES_H
+#endif 
